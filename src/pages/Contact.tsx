7a3c58e4fa9022abf1cd34b63b25d7417913695c
mahfuz2411 <mahfuzibnesyful24@gmail.com>
2026-03-31 03:12:31 +0600

    fix: update social media links to include full URLs for Facebook and Instagram

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -77,7 +77,7 @@ const Contact = () => {
       name: 'Facebook',
       icon: Facebook,
       link: '#',
-      value: 'facebook.com/mahfuzibnesyful',
+      value: 'https://www.facebook.com/mahfuzibnesyful',
       iconColor: 'text-blue-600'
     },
     {
@@ -85,7 +85,7 @@ const Contact = () => {
       name: 'Instagram',
       icon: Instagram,
       link: '#',
-      value: 'instagram.com/mahfuz_ibne_syful',
+      value: 'https://www.instagram.com/mahfuz_ibne_syful/',
       iconColor: 'text-pink-500'
     }
   ];
